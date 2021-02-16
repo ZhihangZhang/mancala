979bfc6737ef08cad63d0133ccbb850efb64a09c
ZhihangZhang <462143015zzh@gmail.com>
2021-02-16 12:53:58 -0800

    update comment

--- Mancala.hs
+++ Mancala.hs
@@ -8,7 +8,7 @@ module Mancala where
 type Board = ([Int], [Int]) -- number of stones for two players, including stores 
 type Store = (Player, Int) -- index of a player's store in [Int]
 type InternalState = (Board, Store, Store) 
-data Action = Action Int                          
+data Action = Action Int -- index of a selected pocket (non-empty)
          deriving (Ord,Eq)
 data State = State InternalState ([Action], [Action])  
          deriving (Ord, Eq, Show)
@@ -31,11 +31,12 @@ instance Show Action where
 -- Thoughts on implementation:
 --
 -- mancala :: Game 
--- - move stones counter-clockwise + possibly capture
---      - empty the selected pocket
---      - add one to following pockets (include the player's own store, but
+-- - move stones counterclockwise + possibly capture
+--      - empty the selected pocket (aka Action)
+--      - update stone count in pockets couterclockwise (include the player's own store, but
 --      skip opponent's)
---      - possibly capture opponent's stones 
+--      - possibly capture opponent's stones if the last stone ends up in an
+--      empty pocket on our side
 -- - update available actions for both players (aka indices of non-empty pockets)
 -- - determine the result of the turn
 --      - end of game? (remeber to collect any remaining pieces)
@@ -47,7 +48,7 @@ instance Show Action where
 -- 
 -- mm_player :: Player
 -- - pretty much the same as Minimax, the only difference being a player can
---      take multiple turns (thus we need to the value function)
+--      take multiple turns (thus we need to change the value function)
 --
 -- IO stuff
 -- - we can modify Play.hs so that a player can take multiple turns
