0e07fafb3909baf9b72c1ce0668941d6d6fdbc69
ZhihangZhang <462143015zzh@gmail.com>
2021-02-23 22:41:37 -0800

    implement Game and Play

--- Mancala.hs
+++ Mancala.hs
@@ -1,4 +1,5 @@
 module Mancala where
+import Data.Tuple (swap)
 
 -- To run it, try:
 -- ghci
@@ -7,31 +8,67 @@ module Mancala where
 
 -- https://endlessgames.com/wp-content/uploads/Mancala_Instructions.pdf
 type Side = [Int] -- last element being the store/mancala
-type Board = (Side, Side)  -- TODO: not sure if using just a list is any better
+type Board = (Side, Side)
 type InternalState = Board 
 data Action = Action Int -- index of a selected pocket (non-empty)
          deriving (Ord,Eq)
 data State = State InternalState ([Action], [Action])
          deriving (Ord, Eq, Show)
 
-data Result = EndOfGame Double State    -- end of game: value, starting state
+data Result = EndOfGame Int State    -- end of game: value, starting state
             | EndOfTurn State        -- the other player continues 
-            | ContinueTurn State        -- current player takes another turn -- might not be necessary. can just call person_play or opponent_play again
+            | ContinueTurn State        -- current player takes another turn 
          deriving (Eq, Show)
 type Game = Action -> State -> Result
 type Player = State -> Action
 
+-- Start states
 side_start = [4, 4, 4, 4, 4, 4, 0]
-actions_start = [Action i | i <- [0..5]] -- only not-empty pockets are valid actions -> Guarding for in play (probably not the best idea) 
-mancala_start = State (side_start, side_start) (actions_start, actions_start) 
+board_start = (side_start, side_start)
+actions_start = avail_actions board_start 
+mancala_start = State board_start actions_start
 
-
--- TODO: write out preconditions for functions
 --mancala :: Game
---mancala move (State ((my_pockets, my_store), (other_pockets, other_store)) (my_actions, other_actions))
--- - move pieces
+mancala move (State (my_side, other_side) (my_actions, other_actions))
+  | end = EndOfGame (win b3) mancala_start
+  | extra_turn = ContinueTurn (State b3 avail) -- extra turn, no need to swap sides and available actions
+  | otherwise = EndOfTurn (State (swap b3) (swap avail)) -- opponent's turn, swap sides and available actions 
+  where 
+    (b1, end_index) = move_pieces move (my_side, other_side) 
+    b2 = capture_pieces end_index (my_side, other_side) b1 
+    (b3, end) = collect_pieces b2
+    avail = avail_actions b3 
+    extra_turn = end_index == (length $ get_pockets my_side)
+-- Tests
+-- 1. end of turn
+-- *Mancala> mancala_start
+-- State ([4,4,4,4,4,4,0],[4,4,4,4,4,4,0]) ([0,1,2,3,4,5],[0,1,2,3,4,5])
+-- *Mancala> r1 = mancala (Action 1) mancala_start
+-- *Mancala> r1
+-- EndOfTurn (State ([4,4,4,4,4,4,0],[4,0,5,5,5,5,0]) ([0,1,2,3,4,5],[0,2,3,4,5]))
+--
+-- 2. extra of turn because the last piece is dropped in the store
+-- *Mancala> mancala_start
+-- State ([4,4,4,4,4,4,0],[4,4,4,4,4,4,0]) ([0,1,2,3,4,5],[0,1,2,3,4,5])
+-- *Mancala> r1 = mancala (Action 2) mancala_start
+-- *Mancala> r1
+-- ContinueTurn (State ([4,4,0,5,5,5,1],[4,4,4,4,4,4,0]) ([0,1,3,4,5],[0,1,2,3,4,5]))
+--
+-- 3. capture opponent's pieces because the last piece is dropeed in an empty
+-- pocket
+-- *Mancala> s1 = (State ([4,4,0,5,0,5,1],[4,5,4,4,4,4,0]) (actionize [0,1,3,5],actionize [0,1,2,3,4,5]))
+-- *Mancala> mancala (Action 0) s1
+-- EndOfTurn (State ([4,0,4,4,4,4,0],[0,5,1,6,0,5,7]) ([0,2,3,4,5],[1,2,3,5]))
+--
+-- 4. capture even if the opposite pocket is empty
+-- *Mancala> s1 = (State ([4,4,0,5,0,5,1],[4,0,4,4,4,4,0]) (actionize [0,1,3,5],actionize [0,1,2,3,4,5]))
+-- *Mancala> mancala (Action 0) s1
+-- EndOfTurn (State ([4,0,4,4,4,4,0],[0,5,1,6,0,5,2]) ([0,2,3,4,5],[1,2,3,5]))
 
---precondition: index >= 0
+
+-- get the number of pieces in a pocket, 0-index starting from the left most
+-- pocket on my side and excluding the opponent's mancala 
+-- precondition: 0 <= index <= 2 * number of pockets
 get_val :: Board -> Int -> Int
 get_val board index = (to_list board) !! index
 {-
@@ -47,6 +84,9 @@ get_val board index = (to_list board) !! index
 *Mancala> get_val myBoard 8
 9
 -}
+
+-- set the number of pieces in a pocket, indexed similarly as get_val
+-- precondition: 0 <= index <= 2 * number of pockets
 set_val :: Board -> Int -> Int -> Board
 set_val (my_side, other_side) index value = to_board new_l other_store len
   where
@@ -61,10 +101,12 @@ set_val (my_side, other_side) index value = to_board new_l other_store len
 *Mancala> set_val myBoard 0 100
 ([100,2,3,4,5,6,7],[8,9,10,11,12,13,14])
 *Mancala> set_val myBoard 12 100
-([1,2,3,4,5,6,7],[8,9,10,11,12,100,14]) -- if used on last index, will add a value
+([1,2,3,4,5,6,7],[8,9,10,11,12,100,14])
 *Mancala> set_val myBoard 2 100
 ([1,2,100,4,5,6,7],[8,9,10,11,12,13,14])
 -}
+
+-- get pockets of a side
 get_pockets :: Side -> [Int]
 get_pockets s = init s
 {-
@@ -74,6 +116,8 @@ get_pockets s = init s
 *Mancala> get_pockets mySide
 [1,2,3,4,5,6]
 -}
+
+-- get the store/mancala of a side
 get_store :: Side -> Int
 get_store s = last s
 {-
@@ -83,6 +127,9 @@ get_store s = last s
 *Mancala> get_store mySide
 7
 -}
+
+-- convert the board to a list representation from my perspective (excluding
+-- the opponent's store/mancala)
 to_list :: Board -> [Int]
 to_list (my_side, other_side) = my_side ++ (get_pockets other_side) 
 {-
@@ -95,6 +142,10 @@ to_list (my_side, other_side) = my_side ++ (get_pockets other_side)
 [1,2,3,4,5,6,7,8,9,10,11,12,13]
 -}
 
+-- convert a list representation to board
+-- l: list presentation of the board from my perspective (result of to_list)
+-- other_store: opponent's store/mancala
+-- len: the number of pockets on my side
 to_board :: [Int] -> Int -> Int -> Board 
 to_board l other_store len = 
   (my_side, other_side)
@@ -120,7 +171,6 @@ to_board l other_store len =
 
 -- Map a list in a circular manner starting from 
 -- a specific position for a certain number of times
--- This will be used to update board state
 circular_map :: [a] -> Int -> Int -> (a -> a) -> [a]
 circular_map [] _ _ _ = []
 circular_map l _ 0 _ = l
@@ -154,33 +204,35 @@ circular_map l start n f
 
 
 
-move_pieces :: Action -> Board -> Board --needs to also return the index of the last dropped element to make life easier
+-- Given an action, move pieces around the board and return the end index and
+-- the updated board
+move_pieces :: Action -> Board -> (Board, Int) 
 move_pieces (Action index) (my_side, other_side) =
-  to_board l other_store len
+  (new_board, end_index)
     where
       other_store = get_store other_side
       len = length $ get_pockets my_side
       val = get_val (my_side, other_side) index
       zeroed_board = set_val (my_side, other_side) index 0 
       l = circular_map (to_list zeroed_board) (index+1) val (+1)
-{-
-*Mancala> myBoard
-([1,2,3,4,5,6,7],[8,9,10,11,12,13,14])
-*Mancala> move_pieces (Action 0) myBoard
-([0,3,3,4,5,6,7],[8,9,10,11,12,13,14])
-*Mancala> move_pieces (Action 1) myBoard
-([1,0,4,5,5,6,7],[8,9,10,11,12,13,14])
-*Mancala> move_pieces (Action 2) myBoard
-([1,2,0,5,6,7,7],[8,9,10,11,12,13,14])
-*Mancala> move_pieces (Action 3) myBoard
-([1,2,3,0,6,7,8],[9,9,10,11,12,13,14])
-*Mancala> move_pieces (Action 4) myBoard
-([1,2,3,4,0,7,8],[9,10,11,11,12,13,14])
-*Mancala> move_pieces (Action 5) myBoard
-([1,2,3,4,5,0,8],[9,10,11,12,13,13,14])
--}
+      new_board = to_board l other_store len
+      end_index = mod (index + val) (length l)
+-- Tests
+-- *Mancala> myBoard = ([1..7], [8..14])
+-- *Mancala> myBoard
+-- ([1,2,3,4,5,6,7],[8,9,10,11,12,13,14])
+-- *Mancala> move_pieces (Action 0) myBoard
+-- (([0,3,3,4,5,6,7],[8,9,10,11,12,13,14]),1)
+-- *Mancala> move_pieces (Action 2) myBoard
+-- (([1,2,0,5,6,7,7],[8,9,10,11,12,13,14]),5)
+-- *Mancala> move_pieces (Action 4) myBoard
+-- (([1,2,3,4,0,7,8],[9,10,11,11,12,13,14]),9)
+-- *Mancala> move_pieces (Action 5) myBoard
+-- (([1,2,3,4,5,0,8],[9,10,11,12,13,13,14]),11)
 
 
+-- Given the end index of a move, capture pieces on a board, if any
+-- assuming two sides have the same number of pockets
 capture_pieces :: Int -> Board -> Board -> Board
 capture_pieces end_index (my_old_side, other_old_side) (my_side, other_side)
   | end_index < len && old_val == 0 = b3
@@ -189,65 +241,129 @@ capture_pieces end_index (my_old_side, other_old_side) (my_side, other_side)
     len = length $ get_pockets my_side 
     old_val = get_val (my_old_side, other_old_side) end_index
     old_store = get_store my_old_side
-    capture_index = 2 * len - end_index
+    capture_index = 2 * len - end_index 
     capture_val = get_val (my_side, other_side) capture_index
     b1 = set_val (my_side, other_side) capture_index 0
     b2 = set_val b1 end_index 0
     b3 = set_val b2 len (capture_val+1+old_store)
--- TODO: tests
-
--- Some preliminary tests
--- 1. testing move and valid capture
--- *Mancala> b1 = move_pieces (Action 4) (side_start, side_start)
+-- Tests
+-- 1. valid capture
+-- *Mancala> b1 = fst $ move_pieces (Action 4) (side_start, side_start)
 -- *Mancala> b1
 -- ([4,4,4,4,0,5,1],[5,5,4,4,4,4,0])
--- *Mancala>
--- *Mancala> b1
--- ([4,4,4,4,0,5,1],[5,5,4,4,4,4,0])
--- *Mancala> b2 = move_pieces (Action 0) b1
+-- *Mancala> b2 = fst $ move_pieces (Action 0) b1
 -- *Mancala> b2
 -- ([0,5,5,5,1,5,1],[5,5,4,4,4,4,0])
+-- *Mancala> snd $ move_pieces (Action 0) b1
+-- 4
 -- *Mancala> capture_pieces 4 b1 b2
 -- ([0,5,5,5,0,5,7],[5,0,4,4,4,4,0])
--- *Mancala>
--- 2. testing invalid capture
--- *Mancala> b1 = move_pieces (Action 2) (side_start, side_start)
+--
+-- 2. invalid capture (expecting no-op)
+-- *Mancala> b1 = fst $ move_pieces (Action 2) (side_start, side_start)
 -- *Mancala> b1
 -- ([4,4,0,5,5,5,1],[4,4,4,4,4,4,0])
+-- *Mancala> snd $ move_pieces (Action 2) (side_start, side_start)
+-- 6
 -- *Mancala> capture_pieces 6 (side_start, side_start) b1
 -- ([4,4,0,5,5,5,1],[4,4,4,4,4,4,0])
--- *Mancala> b1 = move_pieces (Action 4) (side_start, side_start)
+--
+-- *Mancala> b1 = fst $ move_pieces (Action 4) (side_start, side_start)
 -- *Mancala> b1
 -- ([4,4,4,4,0,5,1],[5,5,4,4,4,4,0])
+-- *Mancala> snd $ move_pieces (Action 4) (side_start, side_start)
+-- 8
 -- *Mancala> capture_pieces 8 (side_start, side_start) b1
 -- ([4,4,4,4,0,5,1],[5,5,4,4,4,4,0])
 
 
---update_available_actions :: Board -> ([Action], [Action])
-
-win :: Action -> Board -> Bool
-win (Action n) board 
-  | oneSideClear boardAfter = myStore > oppStore
-  | otherwise  = False 
-  where
-   boardAfter = move_pieces (Action n) board
-   finalCount = cleanBoard boardAfter
-   myStore = fst finalCount
-   oppStore = snd finalCount
-
-oneSideClear :: Board -> Bool
-oneSideClear b = all (== 0) (init(fst b)) || all (== 0) (init(snd b))
-
-cleanBoard :: Board -> (Int, Int)
-cleanBoard b = ((sumSide (fst b)), sumSide (snd b))
-
-sumSide :: Side -> Int
-sumSide s = foldr (+) 0 s
+-- Determine if a game ends or not and collect pieces on the board, if any
+-- return the updated board and a bool indicating whether any pieces are
+-- collected because the game ends
+collect_pieces :: Board -> (Board, Bool)
+collect_pieces board
+  | one_side_clear board = (collect_pieces_helper board, True)
+  | otherwise = (board, False)
+-- Tests
+-- *Mancala> b1 = (side_start, side_start)
+-- *Mancala> b1
+-- ([4,4,4,4,4,4,0],[4,4,4,4,4,4,0])
+-- *Mancala> collect_pieces b1
+-- (([4,4,4,4,4,4,0],[4,4,4,4,4,4,0]),False)
+-- *Mancala> b1 = (replicate 5 0 ++ [4], side_start)
+-- *Mancala> b1
+-- ([0,0,0,0,0,4],[4,4,4,4,4,4,0])
+-- *Mancala> collect_pieces b1
+-- (([0,0,0,0,0,4],[0,0,0,0,0,24]),True)
  
+-- Helper to collect pieces
+collect_pieces_helper :: Board -> Board 
+collect_pieces_helper (my_side, other_side) = updated_board
+    where
+      len = length $ get_pockets my_side
+      zeros = replicate len 0
+      my_side_sum = sum my_side
+      other_side_sum = sum other_side 
+      updated_board = (zeros ++ [my_side_sum], zeros ++ [other_side_sum])
+-- Tests
+-- *Mancala> b1 = (side_start, side_start)
+-- *Mancala> b1
+-- ([4,4,4,4,4,4,0],[4,4,4,4,4,4,0])
+-- *Mancala> collect_pieces_helper b1
+-- ([0,0,0,0,0,0,24],[0,0,0,0,0,0,24])
+-- *Mancala> b2 = (replicate 7 0, replicate 7 0)
+-- *Mancala> b2
+-- ([0,0,0,0,0,0,0],[0,0,0,0,0,0,0])
+-- *Mancala> collect_pieces_helper b2
+-- ([0,0,0,0,0,0,0],[0,0,0,0,0,0,0])
+
+
+-- Determine if one side is already cleared
+one_side_clear :: Board -> Bool
+one_side_clear (my_side, other_side) = 
+  all (== 0) (get_pockets my_side) || all (== 0) (get_pockets other_side)
+-- TODO: tests
+
+-- Determine who wins given a board that has been fully updated (i.e., after
+-- pieces are moved, captured, and collected) 
+-- return an int, it is positive if I win, or negative if the opponent wins
+--win :: Board -> Int
+win (my_side, other_side) = get_store my_side - get_store other_side
+
+-- Given a board return the available actions
+avail_actions :: Board -> ([Action], [Action])
+avail_actions (my_side, other_side) =
+  (filter_avail my_pockets, filter_avail other_pockets)
+  where 
+    my_pockets = get_pockets my_side
+    other_pockets = get_pockets other_side
+    filter_avail pockets = [Action i | (p, i) <- zip pockets [0..], p > 0]
+-- Tests
+-- Mancala> b1
+-- ([4,4,4,4,4,4,0],[4,4,4,4,4,4,0])
+-- *Mancala> avail_actions b1
+-- ([0,1,2,3,4,5],[0,1,2,3,4,5])
+-- *Mancala> b1 = (replicate 5 0 ++ [4], side_start)
+-- *Mancala> b1
+-- ([0,0,0,0,0,4],[4,4,4,4,4,4,0])
+-- *Mancala> avail_actions b1
+-- ([],[0,1,2,3,4,5])
+
 
 instance Show Action where
     show (Action i) = show i
+instance Read Action where
+    readsPrec i st =  [(Action a,rst) | (a,rst) <- readsPrec i st]
 
+-- Helpers for testing in the repl
+actionize :: [Int] -> [Action]
+actionize = map Action
+
+------- A Player -------
+
+-- a simple player that always chooses the first in the availble actions 
+simple_player :: Player
+simple_player (State _ (avail, _)) = head avail
 
 -- TODO: remove these
 -- Thoughts on implementation:
--- Play.hs
+++ Play.hs
@@ -13,10 +13,10 @@ import Text.Read   (readMaybe)
 import Data.Maybe   (fromJust)
 
 type TournammentState = (Int,Int,Int)   -- wins, losses, ties
+type PlayFn = Game -> Result -> Player -> TournammentState -> IO TournammentState
 
 
 play :: Game -> State -> Player -> TournammentState -> IO TournammentState
-
 play game start_state opponent ts =
   let (wins, losses,ties) = ts in
   do
@@ -25,59 +25,93 @@ play game start_state opponent ts =
       line <- getLine
       if line == "0"
         then
-            person_play game (EndofTurn start_state) opponent ts --Every ContinueGame has been replaced with EndOfGame
+            person_play game (EndOfTurn start_state) opponent ts --Every ContinueGame has been replaced with EndOfGame
         else if line ==  "1"
-             then computer_play game (EndofTurn start_state) opponent ts
+             then computer_play game (EndOfTurn start_state) opponent ts
         else if line == "2"
             then return ts
         else play game start_state opponent ts
 
-person_play :: Game -> Result -> Player -> TournammentState -> IO TournammentState
+person_play :: PlayFn
 -- opponent has played, the person must now play
 person_play game (EndOfGame val start_state) opponent ts =
   do
     newts <- update_tournament_state (-val) ts  -- val is value to computer; -val is value for person
     play game start_state opponent newts
 
-person_play game (EndofTurn state) opponent ts =
+person_play game (EndOfTurn state) opponent ts =
    do
-      let State internal avail = state
-      putStrLn ("State: "++show internal++" choose one of "++show init (fst avail))
+      let State internal (avail, _) = state
+      putStrLn ("State: "++show internal++" choose one of "++show avail)
       line <- getLine
       let action = (readMaybe line :: Maybe Action)
-      if (action == Nothing) || not ((fromJust action) `elem` [0..5]) || not fst(avail)!!(fromJust action) == 0
-        then  -- error; redo
-           person_play game (EndofTurn state) opponent ts
+      if (action == Nothing) || not ((fromJust action) `elem` avail)
+        then do  -- error; redo
+           putStrLn ("Invalid choice, try again...")
+           person_play game (EndOfTurn state) opponent ts
         else
-		{- if last bead from action lands in a mancala
-			person_play game game (fromJust action) state) opponent ts
-			else
-           computer_play game (game (fromJust action) state) opponent ts
-		 -}
+           coordinate_next_play game (game (fromJust action) state) opponent ts person_play computer_play
+
+person_play game (ContinueTurn state) opponent ts =
+   do
+      let State internal (avail, _) = state
+      putStrLn ("You got an extra turn!")
+      putStrLn ("State: "++show internal++" choose one of "++show avail)
+      line <- getLine
+      let action = (readMaybe line :: Maybe Action)
+      if (action == Nothing) || not ((fromJust action) `elem` avail)
+        then do -- error; redo
+           putStrLn ("Invalid choice, try again...")
+           person_play game (ContinueTurn state) opponent ts
+        else
+           coordinate_next_play game (game (fromJust action) state) opponent ts person_play computer_play
 
 
-computer_play :: Game -> Result -> Player -> TournammentState -> IO TournammentState
+computer_play :: PlayFn
 -- computer_play game current_result opponent ts
 -- person has played, the computer must now play
-computer_play game (EndOfGame val  start_state) opponent ts =
+computer_play game (EndOfGame val start_state) opponent ts =
    do
       newts <- update_tournament_state val ts
       play game start_state opponent newts
 
-computer_play game (EndofTurn state) opponent ts =
+computer_play game (EndOfTurn state) opponent ts =
       let 
           opponent_move = opponent state
+          State internal _ = state
         in
           do
-		  {- if last bead from action lands in a mancala
-				computer_play game (game (fromJust action) state) opponent ts
-			else
-           person_play game game (fromJust action) state) opponent ts
-		 -}
             putStrLn ("The computer chose "++show opponent_move)
-            person_play game (game opponent_move state) opponent ts
+            coordinate_next_play game (game opponent_move state) opponent ts computer_play person_play
 
-update_tournament_state:: Double -> TournammentState -> IO TournammentState
+computer_play game (ContinueTurn state) opponent ts =
+      let 
+          opponent_move = opponent state
+          State internal _ = state
+        in
+          do
+            putStrLn ("Computer got an extra turn!")
+            putStrLn ("The computer chose "++show opponent_move)
+            coordinate_next_play game (game opponent_move state) opponent ts computer_play person_play
+
+-- coordinate the next play by inspecting the result of last play
+coordinate_next_play :: Game -> Result -> Player -> TournammentState -> PlayFn -> PlayFn -> IO TournammentState
+-- game ends, pass to the other player
+coordinate_next_play game (EndOfGame val start_state) opponent ts my_play other_play =
+  do
+    other_play game (EndOfGame val start_state) opponent ts
+
+-- my turn ends, pass to the other player
+coordinate_next_play game (EndOfTurn state) opponent ts my_play other_play =
+  do
+    other_play game (EndOfTurn state) opponent ts
+
+-- I got an extra turn, pass to myself 
+coordinate_next_play game (ContinueTurn state) opponent ts my_play other_play =
+  do
+    my_play game (ContinueTurn state) opponent ts
+
+update_tournament_state:: Int -> TournammentState -> IO TournammentState
 -- given value to the person, the tournament state, return the new tournament state
 update_tournament_state val (wins,losses,ties)
   | val > 0 = do
@@ -90,10 +124,6 @@ update_tournament_state val (wins,losses,ties)
       putStrLn "Computer won!"
       return (wins,losses+1,ties)
 
--- If you imported MagicSum here and in Minimax try:
--- play magicsum magicsum_start simple_player (0,0,0)
--- play magicsum magicsum_start (mm_player magicsum) (0,0,0) -- minimax player
-
--- If you imported CountGame here and in Minimax try:
--- play countGame count_start simple_count_player (0,0,0)
--- play countGame count_start (mm_player countGame) (0,0,0)
+-- If you imported Mancala here and in Minimax try:
+-- play mancala mancala_start simple_player (0,0,0)
+-- TODO: implement Minimax for mancala
